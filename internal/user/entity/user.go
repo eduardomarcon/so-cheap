dcd34b522117f34ca5c907e61c9e1dbb8a288ce2
eduardomarcon <eduardommarcon@gmail.com>
2022-12-04 14:21:32 -0300

    chore: removing unused methods

--- internal/user/entity/user.go
+++ internal/user/entity/user.go
@@ -1,30 +1,7 @@
 package entity
 
-import "errors"
-
 type User struct {
 	ID    int64  `json:"id"`
 	Name  string `json:"name"`
 	Email string `json:"email"`
-}
-
-func NewUser(description string, email string) (*User, error) {
-	item := &User{
-		Name:  description,
-		Email: email,
-	}
-	if err := item.isValid(); err != nil {
-		return nil, err
-	}
-	return item, nil
-}
-
-func (i *User) isValid() error {
-	if i.Name == "" {
-		return errors.New("description invalid")
-	}
-	if i.Email == "" {
-		return errors.New("amount invalid")
-	}
-	return nil
 }
