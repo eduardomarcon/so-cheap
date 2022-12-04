3480339fa21a176dfa4c8657942dc3c01bb345fa
eduardomarcon <eduardommarcon@gmail.com>
2022-12-04 19:56:34 -0300

    chore: renaming packages

--- pkg/route/route.go
+++ pkg/route/route.go
@@ -2,22 +2,22 @@ package route
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"so-cheap/internal/item/entrypoint"
+	itementrypoint "so-cheap/internal/item/entrypoint"
 	userentrypoint "so-cheap/internal/user/entrypoint"
 )
 
 func Routes(a *fiber.App) {
 	route := a.Group("/api/v1")
 
-	route.Post("itens", entrypoint.CreateItem)
-	route.Put("itens/:id", entrypoint.UpdateItem)
-	route.Get("itens/:id", entrypoint.GetItem)
-	route.Get("itens", entrypoint.GetItens)
-	route.Delete("itens/:id", entrypoint.DeleteItem)
-
 	route.Post("users", userentrypoint.CreateUser)
 	route.Put("users/:id", userentrypoint.UpdateUser)
 	route.Get("users/:id", userentrypoint.GetUser)
 	route.Get("users", userentrypoint.GetUsers)
 	route.Delete("users/:id", userentrypoint.DeleteUser)
+
+	route.Post("itens", itementrypoint.CreateItem)
+	route.Put("itens/:id", itementrypoint.UpdateItem)
+	route.Get("itens/:id", itementrypoint.GetItem)
+	route.Get("itens", itementrypoint.GetItens)
+	route.Delete("itens/:id", itementrypoint.DeleteItem)
 }
